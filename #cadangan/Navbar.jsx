1ad2c0ccd520c7ab2def10aa9a2b2731671ea08f
Zianatul Muna <zianatulmn@gmail.com>
2024-01-02 20:26:44 +0700

    responsive & navbar fix

--- #cadangan/Navbar.jsx
+++ #cadangan/Navbar.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
-import { LuAlignJustify } from "react-icons/lu";
 
 function Navbar() {
   const [changeColor, setChangeColor] = useState(false);
@@ -36,22 +35,18 @@ function Navbar() {
     //     </ul>
     //   </nav>
     // </header>
-    <nav class={`navbar navbar-expand-md ${changeColor ? 'navbar-header py-2' : 'navbar-header-trans py-4' }  px-3 px-sm-4 px-md-5 px-xl-9 sticky-top`}>
-      <div class="container-fluid p-0">
-        {/* <a class="" href="#">Navbar</a> */}
-        <div className='navbar-brand hstack gap-3'>
-          <img src="/assets/zian.png" className='logo' alt="" />
-          <h5 className='d-none d-sm-block'>ZIANATUL MUNA</h5>
-        </div>
-        <button class="navbar-toggler border-0 fs-1" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <LuAlignJustify />
+    <nav class={`navbar navbar-expand-lg ${changeColor ? 'navbar-header px-3 px-sm-4 px-md-5 px-xl-9 py-2' : '' } sticky-top bg-body-tertiary`}>
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
-          <div class="navbar-nav py-3 py-md-0 text-center gap-2 gap-md-4 gap-xl-5">
-            <a class="nav-link" aria-current="page" href="#home">HOME</a>
-            <a class="nav-link" href="#about">ABOUT</a>
-            <a class="nav-link" href="#contact">CONTACT</a>
-            <a class="btn btn-root-secondary px-3 py-2 rounded-5 text-dark" href='#project'>PROJECT</a>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link" href="#">Features</a>
+            <a class="nav-link" href="#">Pricing</a>
+            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
           </div>
         </div>
       </div>
